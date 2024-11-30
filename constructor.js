c8ec1f4b281ee44ad0bbf1f7f05bcb6a144d106d
Islam Atwa <Islam Atwa>
2024-11-30 11:20:55 +0200

    tasl constructoe manara

---
+++ constructor.js
@@ -0,0 +1,21 @@
+// Write a constructor for making “Book” objects.
+//  We will revisit this in the next project. 
+//  Your book objects should have the book’s title, author, the number of pages, and whether or not you have read the book.
+
+
+function Book (title, author, numPage, read){
+    this.title = title;
+    this.author = author;
+    this.numPage = numPage;
+    this.read = read;
+
+    this.info=function(){
+        return `${this.title} by ${this.author}, ${this.numPage} pages, ${this.read ? "already read" : "not read yet"}` ;
+
+    };
+
+}
+
+const Habits = new Book('AtomicHabits','jamesClear', 210, 'Yes');
+
+console.log(Habits.info());
