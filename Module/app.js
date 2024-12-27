7fb22edfe5d67801632e00cb57a7604a5ef8a878
Islam Atwa <Islam Atwa>
2024-12-27 23:46:15 +0200

    Modules in JS

---
+++ Module/app.js
@@ -0,0 +1,6 @@
+
+import {a, arr, sayHello} from "./main.js";
+
+console.log(a); // 10
+console.log(arr); // 10
+console.log(sayHello()); // Hello
---
+++ Module/main.js
@@ -0,0 +1,8 @@
+
+
+export let a = 10;
+export let arr = [1, 2, 3, 4, 5];
+
+export function sayHello() {
+    return 'Hello';
+}
